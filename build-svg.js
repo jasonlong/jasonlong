98d68569a2e99afa592b8f052908ca219597ec4e
Jason Long <jason@jasonlong.me>
2021-06-02 14:14:20 -0400

    Switch to accuweather

--- build-svg.js
+++ build-svg.js
@@ -1,60 +1,85 @@
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
-let fs = require('fs')
-let formatDistance = require('date-fns/formatDistance')
-let weather = require('openweather-apis')
-let qty = require('js-quantities')
+let fs = require("fs");
+let got = require("got");
+let qty = require("js-quantities");
+let formatDistance = require("date-fns/formatDistance");
+
+let WEATHER_DOMAIN = "http://dataservice.accuweather.com";
 
 const emojis = {
-  '01d': '☀️',
-  '02d': '⛅️',
-  '03d': '☁️',
-  '04d': '☁️',
-  '09d': '🌧',
-  '10d': '🌦',
-  '11d': '🌩',
-  '13d': '❄️',
-  '50d': '🌫'
-}
+  1: "☀️",
+  2: "☀️",
+  3: "🌤",
+  4: "🌤",
+  5: "🌤",
+  6: "🌥",
+  7: "☁️",
+  8: "☁️",
+  11: "🌫",
+  12: "🌧",
+  13: "🌦",
+  14: "🌦",
+  15: "⛈",
+  16: "⛈",
+  17: "🌦",
+  18: "🌧",
+  19: "🌨",
+  20: "🌨",
+  21: "🌨",
+  22: "❄️",
+  23: "❄️",
+  24: "🌧",
+  25: "🌧",
+  26: "🌧",
+  29: "🌧",
+  30: "🌫",
+  31: "🥵",
+  32: "🥶",
+};
 
 // Time working at PlanetScale
-const today = new Date()
-const todayDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(today)
+const today = new Date();
+const todayDay = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
+  today
+);
 
 const psTime = formatDistance(new Date(2020, 12, 14), today, {
-  addSuffix: false
-})
+  addSuffix: false,
+});
 
 // Today's weather
-weather.setLang('en')
-weather.setCoordinate(40.03214310107579, -83.16110854925773)
-weather.setUnits('imperial')
-weather.setAPPID(WEATHER_API_KEY)
+const locationKey = "18363_PC";
+let url = `forecasts/v1/daily/1day/${locationKey}?apikey=${WEATHER_API_KEY}`;
 
-weather.getWeatherOneCall(function (err, data) {
-  if (err) console.log(err)
+got(url, { prefixUrl: WEATHER_DOMAIN })
+  .then((response) => {
+    let json = JSON.parse(response.body);
 
-  const degF = Math.round(data.daily[0].temp.max)
-  const degC = Math.round(qty(`${degF} tempF`).to('tempC').scalar)
-  const icon = data.daily[0].weather[0].icon
+    const degF = Math.round(json.DailyForecasts[0].Temperature.Maximum.Value);
+    const degC = Math.round(qty(`${degF} tempF`).to("tempC").scalar);
+    const icon = json.DailyForecasts[0].Day.Icon;
 
-  fs.readFile('template.svg', 'utf-8', (error, data) => {
-    if (error) {
-      console.error(error)
-      return
-    }
-
-    data = data.replace('{degF}', degF)
-    data = data.replace('{degC}', degC)
-    data = data.replace('{weatherEmoji}', emojis[icon])
-    data = data.replace('{psTime}', psTime)
-    data = data.replace('{todayDay}', todayDay)
-
-    data = fs.writeFile('chat.svg', data, (err) => {
-      if (err) {
-        console.error(err)
-        return
+    fs.readFile("template.svg", "utf-8", (error, data) => {
+      if (error) {
+        return;
       }
-    })
+
+      data = data.replace("{degF}", degF);
+      data = data.replace("{degC}", degC);
+      data = data.replace("{weatherEmoji}", emojis[icon]);
+      data = data.replace("{psTime}", psTime);
+      data = data.replace("{todayDay}", todayDay);
+
+      data = fs.writeFile("chat.svg", data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    });
   })
-})
+  .catch((err) => {
+    // TODO: something better
+    console.log(err);
+  });
