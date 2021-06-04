c9c760318610785772bd0552a9dafc70b64c9e16
Jason Long <jason@jasonlong.me>
2021-06-04 10:05:05 -0400

    Add prettier config

--- build-svg.js
+++ build-svg.js
@@ -1,42 +1,42 @@
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 
-let fs = require("fs");
-let got = require("got");
-let qty = require("js-quantities");
-let formatDistance = require("date-fns/formatDistance");
+let fs = require('fs')
+let got = require('got')
+let qty = require('js-quantities')
+let formatDistance = require('date-fns/formatDistance')
 
-let WEATHER_DOMAIN = "http://dataservice.accuweather.com";
+let WEATHER_DOMAIN = 'http://dataservice.accuweather.com'
 
 const emojis = {
-  1: "☀️",
-  2: "☀️",
-  3: "🌤",
-  4: "🌤",
-  5: "🌤",
-  6: "🌥",
-  7: "☁️",
-  8: "☁️",
-  11: "🌫",
-  12: "🌧",
-  13: "🌦",
-  14: "🌦",
-  15: "⛈",
-  16: "⛈",
-  17: "🌦",
-  18: "🌧",
-  19: "🌨",
-  20: "🌨",
-  21: "🌨",
-  22: "❄️",
-  23: "❄️",
-  24: "🌧",
-  25: "🌧",
-  26: "🌧",
-  29: "🌧",
-  30: "🌫",
-  31: "🥵",
-  32: "🥶",
-};
+  1: '☀️',
+  2: '☀️',
+  3: '🌤',
+  4: '🌤',
+  5: '🌤',
+  6: '🌥',
+  7: '☁️',
+  8: '☁️',
+  11: '🌫',
+  12: '🌧',
+  13: '🌦',
+  14: '🌦',
+  15: '⛈',
+  16: '⛈',
+  17: '🌦',
+  18: '🌧',
+  19: '🌨',
+  20: '🌨',
+  21: '🌨',
+  22: '❄️',
+  23: '❄️',
+  24: '🌧',
+  25: '🌧',
+  26: '🌧',
+  29: '🌧',
+  30: '🌫',
+  31: '🥵',
+  32: '🥶',
+}
 
 // Cheap, janky way to have variable bubble width
 dayBubbleWidths = {
@@ -47,51 +47,52 @@ dayBubbleWidths = {
   Friday: 220,
   Saturday: 245,
   Sunday: 230,
-};
+}
 
 // Time working at PlanetScale
-const today = new Date();
-const todayDay = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
+const today = new Date()
+const todayDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(
   today
-);
+)
 
 const psTime = formatDistance(new Date(2020, 12, 14), today, {
   addSuffix: false,
-});
+})
 
 // Today's weather
-const locationKey = "18363_PC";
-let url = `forecasts/v1/daily/1day/${locationKey}?apikey=${WEATHER_API_KEY}`;
+const locationKey = '18363_PC'
+let url = `forecasts/v1/daily/1day/${locationKey}?apikey=${WEATHER_API_KEY}`
 
 got(url, { prefixUrl: WEATHER_DOMAIN })
   .then((response) => {
-    let json = JSON.parse(response.body);
+    console.log(response.body)
+    let json = JSON.parse(response.body)
 
-    const degF = Math.round(json.DailyForecasts[0].Temperature.Maximum.Value);
-    const degC = Math.round(qty(`${degF} tempF`).to("tempC").scalar);
-    const icon = json.DailyForecasts[0].Day.Icon;
+    const degF = Math.round(json.DailyForecasts[0].Temperature.Maximum.Value)
+    const degC = Math.round(qty(`${degF} tempF`).to('tempC').scalar)
+    const icon = json.DailyForecasts[0].Day.Icon
 
-    fs.readFile("template.svg", "utf-8", (error, data) => {
+    fs.readFile('template.svg', 'utf-8', (error, data) => {
       if (error) {
-        return;
+        return
       }
 
-      data = data.replace("{degF}", degF);
-      data = data.replace("{degC}", degC);
-      data = data.replace("{weatherEmoji}", emojis[icon]);
-      data = data.replace("{psTime}", psTime);
-      data = data.replace("{todayDay}", todayDay);
-      data = data.replace("{dayBubbleWidth}", dayBubbleWidths[todayDay]);
+      data = data.replace('{degF}', degF)
+      data = data.replace('{degC}', degC)
+      data = data.replace('{weatherEmoji}', emojis[icon])
+      data = data.replace('{psTime}', psTime)
+      data = data.replace('{todayDay}', todayDay)
+      data = data.replace('{dayBubbleWidth}', dayBubbleWidths[todayDay])
 
-      data = fs.writeFile("chat.svg", data, (err) => {
+      data = fs.writeFile('chat.svg', data, (err) => {
         if (err) {
-          console.error(err);
-          return;
+          console.error(err)
+          return
         }
-      });
-    });
+      })
+    })
   })
   .catch((err) => {
     // TODO: something better
-    console.log(err);
-  });
+    console.log(err)
+  })
